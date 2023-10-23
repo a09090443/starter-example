23d94763c9b3c7b8192b7dce71b752e23a33a1ba
Zipe <zipe.daden@gmail.com>
2023-10-23 16:46:12 +0800

    1. Add webservice examples. 2. Add webservice test request file.

--- src/main/java/com/example/webservice/ExampleWebService.java
+++ src/main/java/com/example/webservice/ExampleWebService.java
@@ -1,23 +1,12 @@
 package com.example.webservice;
 
-import com.example.model.User;
-import jakarta.jws.WebMethod;
-import jakarta.jws.WebParam;
-import jakarta.jws.WebResult;
-
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
-import java.util.Map;
 
-@WebService(targetNamespace = "http://service.example.com")
+@WebService(targetNamespace = "http://service.example.com/")
 public interface ExampleWebService {
 
     @WebMethod//標注該方法為webservice暴露的方法,用於向外公布，它修飾的方法是webservice方法，去掉也沒影響的，類似一個注釋信息。
-    public User getUser(@WebParam(partName = "userName") String userName);
-
-    @WebMethod
-    public String getUserGender(String userName);
-
-    @WebMethod
-    @WebResult(name = "Map")
-    public Map<String, User> getAllUserData();
+    public String sayHello(@WebParam(name = "name", targetNamespace = "http://service.example.com/") String name);
 }
--- src/main/java/com/example/webservice/impl/ExampleWebServiceImpl.java
+++ src/main/java/com/example/webservice/impl/ExampleWebServiceImpl.java
@@ -2,52 +2,25 @@ package com.example.webservice.impl;
 
 import com.example.model.User;
 import com.example.webservice.ExampleWebService;
-import jakarta.jws.WebParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.jws.WebService;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @WebService(serviceName = "ExampleWebService",//對外發布的服務名
-        targetNamespace = "http://service.example.com",//指定你想要的名稱空間，通常使用使用包名反轉
+        targetNamespace = "http://service.example.com/",//指定你想要的名稱空間，通常使用使用包名反轉
         endpointInterface = "com.example.webservice.ExampleWebService")
-//服務接口全路徑, 指定做SEI（Service EndPoint Interface）服務端點接口
 @Component
 public class ExampleWebServiceImpl implements ExampleWebService {
 
-    private final Logger logger = LoggerFactory.getLogger(ExampleWebServiceImpl.class);
     private final Map<String, User> userMap = new HashMap<>();
 
-    public ExampleWebServiceImpl() {
-        logger.info("向實體類插入數據");
-        User user = new User("Gary", "M");
-        userMap.put(user.getName(), user);
-
-        user = new User("Mary", "F");
-        userMap.put(user.getName(), user);
-
-        user = new User("Cindy", "F");
-        userMap.put(user.getName(), user);
-    }
-
     @Override
-    public String getUserGender(@WebParam(partName = "userName", targetNamespace = "http://service.example.com") String userName) {
-        logger.info("userName：{}", userMap.get(userName).getGender());
-        return userMap.get(userName).getGender();
+    public String sayHello(String name) {
+        log.info("Hello, {}!", name);
+        return "Hello, " + name + "!";
     }
-
-    @Override
-    public User getUser(String userName) {
-        logger.info("userMap是:{}", userMap);
-        return userMap.get(userName);
-    }
-
-    @Override
-    public Map<String, User> getAllUserData() {
-        return userMap;
-    }
-
 }
